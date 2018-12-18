e07608becf1107a4089fdc354afd17322b0a4292
snowleo <393394595@qq.com>
2018-12-19 03:02:03 +0800

    修复puppeteer在ubuntu无法运行的问题

--- yunyang_server/crawler/util/handle.js
+++ yunyang_server/crawler/util/handle.js
@@ -12,7 +12,7 @@ module.exports = {
     initCrawler(params = {}) {
         return new Promise(async(resolve, reject) => {
             let reload_count = 5;
-            const browser = await puppeteer.launch();
+            const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
             const page = await browser.newPage();
             await page.setUserAgent(params.userAgent || 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)' +
                     ' Chrome/69.0.3497.100 Safari/537.36');
